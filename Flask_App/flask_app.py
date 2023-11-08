c1d3d175771847574f194a826d5a07f4f527ea7e
V.Romain <romain.vuga@student.odisee.be>
2023-11-08 11:40:14 +0100

    adding to Flask app

--- Flask_App/flask_app.py
+++ Flask_App/flask_app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import redirect
+from flask import url_for
 import datetime
 import folium
+
+# Hard-coded user credentials for demonstration
+valid_username = "admin"
+valid_password = "password"
 
 microweb_app = Flask(__name__)
 
@@ -24,5 +30,27 @@ def map():
     # Create an HTML template with the map
     return render_template('map.html', map_html=map_html)
 
+@microweb_app.route("/time")
+def time():
+    return render_template("time.html")
+
+@microweb_app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == valid_username and password == valid_password:
+            # In a real application, you would create a user session here.
+            return redirect(url_for('main'))
+        else:
+            return "Invalid username or password"
+
+    return render_template('login.html')
+
+@microweb_app.route("/create")
+def create():
+    return render_template("create.html")
+
 if __name__ == "__main__":
-    microweb_app.run(host="127.0.0.1", port=5050)
+    microweb_app.run(host="127.0.0.1", port=5563)
